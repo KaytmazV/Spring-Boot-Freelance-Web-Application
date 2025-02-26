665fb314530976713deb87375b57d5f0cc8a072a
volkankaytmaz <vkaytmaz@gmail.com>
2025-02-26 15:41:23 +0300

    V.0.0.0

--- src/main/java/com/volkankaytmaz/backendproject2/Entity/Customers.java
+++ src/main/java/com/volkankaytmaz/backendproject2/Entity/Customers.java
@@ -1,14 +1,13 @@
 package com.volkankaytmaz.backendproject2.Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 @Entity
-public class customers {
+public class Customers {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +16,8 @@ public class customers {
     private String name;
     private String phoneNumber;
 
+    @OneToMany(mappedBy = "customers", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Treatment> treatments;
 
 
     public Long getId() {
--- src/main/java/com/volkankaytmaz/backendproject2/Entity/Treatment.java
+++ src/main/java/com/volkankaytmaz/backendproject2/Entity/Treatment.java
@@ -1,21 +1,49 @@
 package com.volkankaytmaz.backendproject2.Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 
 public class Treatment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
-    private int id;
+    private Long id;
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Customers customer;
 
+    public Long getID() {
+        return id;
+    }
+    public void setID(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(optional = false)
+    private Customers customers;
+
+    public Customers getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Customers customers) {
+        this.customers = customers;
+    }
 }
