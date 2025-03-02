71ea226089f100936ddbd4ae9b2b10d4f30a31c2
volkankaytmaz <vkaytmaz@gmail.com>
2025-03-02 14:59:20 +0300

    V.0.0.1

---
+++ src/main/java/com/volkankaytmaz/backendproject2/configs/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.volkankaytmaz.backendproject2.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+
+
+}
